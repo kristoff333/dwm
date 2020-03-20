ebab506c251e0cdc6df9db91ad7915017f8aeea1
Kristoff Avermaete <kristoff.avermaete@vanhool.com>
2020-03-20 16:16:06 +0100

    config: add lock and some UX tweaks
    - specify monitor for known programs
    - super-shift-l --> xscreensaver
    - default master screen width is 50%
    - unrelated: inherit ENV CFLAGS for build

--- config.h
+++ config.h
@@ -55,12 +55,12 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "GNU Image",     NULL,       NULL,       1 << 7,       1,           0 },
+	{ "firefox",  NULL,       NULL,       1 << 8,       0,           1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.40; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
@@ -91,12 +91,14 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *lockcmd[]  = { "xscreensaver-command", "-lock", NULL };
 
 #include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lockcmd } },
 	{ MODKEY,			XK_q,		killclient,	{0} },
 	{ MODKEY,                       XK_j,		focusstack,	{.i = +1 } },
 	{ MODKEY,                       XK_k,		focusstack,	{.i = -1 } },
